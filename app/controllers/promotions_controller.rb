2eb335aadd1f0aeac1976504bfe01921239f4d4f
RCOliveira98 <rcoliveira.ti@gmail.com>
2021-01-28 08:43:47 -0300

    Permitir que administrador cadastre uma nova promoção

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,9 +1,28 @@
 class PromotionsController < ApplicationController
+
     def index
-        @promotions = Promotion.all
+        @promotions = Promotion.all()
+    end
+
+    def new
+        @promotion = Promotion.new()
+    end
+
+    def create
+        @promotion = Promotion.new(promotion_params())
+        if @promotion.save()
+            redirect_to promotion_path(id: @promotion.id)
+        else
+            render :new
+        end
     end
 
     def show
         @promotion = Promotion.find(params[:id])
+    end
+
+    private
+    def promotion_params
+        params.require(:promotion).permit(:name, :description, :code, :discount_rate, :expiration_date, :coupon_quantity)
     end
 end
