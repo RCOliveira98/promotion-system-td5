9cd43af545e2279fd1265022df982fd2fe4c9096
RCOliveira98 <rcoliveira.ti@gmail.com>
2021-02-08 14:48:21 -0300

    Admin inactivate coupon: hide button inactive

--- spec/features/admin_inactivate_coupon_spec.rb
+++ spec/features/admin_inactivate_coupon_spec.rb
@@ -15,4 +15,33 @@ feature 'Admin inactivate coupon' do
 
         expect(page).to have_content("#{coupon.code} (Inativo)")
     end
+
+    scenario 'does not view button' do
+        promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+            code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+            expiration_date: '22/12/2033')
+
+        coupon_active = Coupon.create!(code: 'NATAL10-0001', promotion: promotion, status: :active)
+
+        coupon_inactive = Coupon.create!(code: 'NATAL10-0002', promotion: promotion, status: :inactive)
+
+        visit root_path
+        click_on 'Promoções'
+        click_on promotion.name
+
+        expect(page).to have_content('NATAL10-0001 (Ativo)')
+        expect(page).to have_content('NATAL10-0002 (Inativo)')
+
+        within("tr#coupon_#{coupon_active.id}") do
+            expect(page).to have_link 'Inativar'
+        end
+
+        within("tr#coupon_#{coupon_inactive.id}") do
+            expect(page).not_to have_link 'Inativar'
+        end
+
+        # expect(page).to have_css("tr#coupon-#{coupon_active.id}")
+
+    end
+
 end
